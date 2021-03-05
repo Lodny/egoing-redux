b64723125a412b5538737c6440d4b260a4a5bc86
lodny03 <drinkjuice@naver.com>
2021-03-05 10:42:12 +0900

    5th commit - branch : react-redux - apply react-redux

--- src/container/AddNumber.js
+++ src/container/AddNumber.js
@@ -1,9 +1,16 @@
-import { Component } from "react";
-import store from "../store";
+// import { Component } from "react";
+// import store from "../store";
 import AddNumber from "../components/AddNumber";
+import { connect } from "react-redux";
 
-export default class Wrapped extends Component {
-  render() {
-    return <AddNumber onClick={(size) => store.dispatch({ type: "INC", number: size })}></AddNumber>;
-  }
+function mapReductDispatch2ReactProps(dispatch) {
+  return { onClick: (size) => dispatch({ type: "INC", number: size }) };
 }
+
+export default connect(null, mapReductDispatch2ReactProps)(AddNumber);
+
+// export default class Wrapped extends Component {
+//   render() {
+//     return <AddNumber onClick={(size) => store.dispatch({ type: "INC", number: size })}></AddNumber>;
+//   }
+// }
--- src/container/DisplayNumber.js
+++ src/container/DisplayNumber.js
@@ -1,22 +1,33 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
 import store from "../store";
 import DisplayNumber from "../components/DisplayNumber";
+import { connect } from "react-redux";
 
-export default class Wrapped extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      number: store.getState().number,
-    };
-  }
-
-  componentDidMount() {
-    store.subscribe(() => {
-      this.setState({ number: store.getState().number });
-    });
-  }
-
-  render() {
-    return <DisplayNumber number={this.state.number}></DisplayNumber>;
-  }
+function mapReduxState2ReactProps(state) {
+  return { number: state.number };
 }
+
+function mapReductDispatch2ReactProps() {
+  return {};
+}
+
+export default connect(mapReduxState2ReactProps, mapReductDispatch2ReactProps)(DisplayNumber);
+
+// export default class Wrapped extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       number: store.getState().number,
+//     };
+//   }
+
+//   componentDidMount() {
+//     store.subscribe(() => {
+//       this.setState({ number: store.getState().number });
+//     });
+//   }
+
+//   render() {
+//     return <DisplayNumber number={this.state.number}></DisplayNumber>;
+//   }
+// }
